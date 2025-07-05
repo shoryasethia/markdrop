f177141559d0a067f72fb7e2ec976b43326251da
Shorya Sethia <132898518+shoryasethia@users.noreply.github.com>
2025-07-05 23:27:40 +0530

    Minor fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="markdrop",
-    version="3.4.0",
+    version="3.5.0",
     packages=find_packages(),
     install_requires=[
         "beautifulsoup4",
